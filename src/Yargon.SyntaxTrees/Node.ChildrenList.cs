5af80ccfc28df80ac55fa64b06a22d0b5d3b622f
Daniel Pelsmaeker <info@virtlink.com>
2016-12-09 00:23:58 +0100

    Fix issue with ChildrenList

--- src/Yargon.SyntaxTrees/Node.ChildrenList.cs
+++ src/Yargon.SyntaxTrees/Node.ChildrenList.cs
@@ -76,40 +76,10 @@ namespace Yargon.SyntaxTrees
                 if (!TryGet(index, out redNode))
                 {
                     var greenNode = this.owner.GreenNode.Children[index];
-                    int offset = CalculateOffset(index);
-                    redNode = this.owner.Factory.Create(greenNode, this.owner, offset);
+                    redNode = this.owner.Factory.Create(greenNode, this.owner, index);
                     this.children[index] = redNode;
                 }
                 return redNode;
-            }
-
-            /// <summary>
-            /// Calculates the offset of the child with the specified index.
-            /// </summary>
-            /// <param name="index">The zero-based index of the child.</param>
-            /// <returns>The zero-based offset.</returns>
-            private int CalculateOffset(int index)
-            {
-                #region Contract
-                Debug.Assert(index >= 0 && index < this.children.Length);
-                #endregion
-                
-                int span = 0;
-                for (int i = index - 1; i >= 0; i--)
-                {
-                    // Add the green node's width to the offset.
-                    var greenNode = this.owner.GreenNode.Children[i];
-                    span += greenNode.Width;
-
-                    // If we find a red node,
-                    // return the final offset immediately.
-                    var redNode = this.children[i];
-                    if (redNode != null)
-                        return redNode.Offset + span;
-                }
-
-                // We didn't find a red child node.
-                return this.owner.Offset + span;
             }
 
             /// <inheritdoc />
